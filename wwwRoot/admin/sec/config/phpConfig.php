deb8f68d2c59db383522a9589d1b889dbf378eea
bitPimps <bitpimps@gmail.com>
2026-02-19 10:13:57 -0600

    Delete wwwRoot/admin/sec/config/phpConfig.php

--- wwwRoot/admin/sec/config/phpConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Database settings
-define('DB_NAME', 'ellisfamdb');
-define('DB_USER', '3116amdb');
-define('DB_PASSWORD', 'f1sht1nk');
-define('DB_HOST', 'ellisfamdb.lixlink.com');
-define('DB_CHARSET', 'utf8');
-?>
